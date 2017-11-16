e0ce02ecb4c1e2c0815d775b9876aafae77dc73a
Rohith09 <kandirohith09@gmail.com>
2017-11-16 20:06:10 +0530

    Added code to learn the use of "self".

--- Class_Ex.py
+++ Class_Ex.py
@@ -1,9 +1,11 @@
 class PartyAnimal:  #Creating a Class
-    x = 0            #Creating a variable
+    x = 0
+    name="Rohith09"  #Creating a variables.
 
     def party(self):            #Declaring a function.Using Self is mandatory in order to access the variables
 
         self.x = self.x + 1
+        print(self.name)
         print("So Far ", self.x)
 
 
